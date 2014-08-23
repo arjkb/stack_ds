db3ca5839cb7673e3912438769a2f47178ffd83c
Arjun_Krishna_Babu <arjunkbabu@users.noreply.github.com>
2014-08-23 14:21:40 +0530

    First Commit

---
+++ stack.cpp
@@ -0,0 +1,125 @@
+/*
+	Program to implement a stack in C++ (using arrays)
+
+	CODER : Arjun Krishna Babu
+	DATE  : 23 - August - 2014
+
+	COMPILER  : G++
+	OS	      : Ubuntu 12.04 LTS
+	PROCESSOR : 4th gen. Intel i5
+
+*/
+
+#include<iostream>
+#define OVERFLOW_MESSAGE  cout<<"\n ERROR: Overflow!"
+#define UNDERFLOW_MESSAGE cout<<"\n ERROR: Underflow!"
+
+using namespace std;
+
+const int SIZE = 5;		//max size of stack
+
+class Stack	{
+private:
+	int A[SIZE];
+	int top;
+	bool isFull(void);	//returns true if the stack is full, false otherwise
+	bool isEmpty(void); //returns true if the stack is empty, false otherwise
+public:
+	Stack(): top(-1) { }
+
+	void push(int data);
+	int  pop(void);	
+	int  peek(void);
+	void dispAll(void);	
+	void clear(void);	//clears the stack (sets top to -1)
+};
+
+int main()	{
+	Stack S;
+	int choice;
+	int item;
+	
+	do	{
+		cout<<"\n 1. Push";
+		cout<<"\n 2. Pop";
+		cout<<"\n 3. Peek";
+		cout<<"\n 4. Display all elements";
+		cout<<"\n 5. Clear Stack";
+		cout<<"\n 0. EXIT";
+		cout<<"\n Enter Your Choice: "; cin>>choice;
+		
+		switch(choice)	{
+			case 1: cout<<"\n Enter an item to push onto stack: ";
+					cin>>item;
+					S.push(item);
+					break;
+			case 2: cout<<"\n Deleting element: "<<S.pop();		
+					break;
+			case 3: cout<<"\n Element at top: "<<S.peek();
+					break;
+			case 4: cout<<"\n The elements in the stack: "; S.dispAll();
+					break;
+			case 5: S.clear();
+					cout<<"\n The stack had been cleared!";
+					break;
+			case 0: cout<<"\n Thank you for using this program!";
+		
+					break;
+		   default: cout<<"\n ERROR: Invalid Choice!";
+		   			break;
+		}
+		cout<<"\n\n";
+	}while(choice != 0);
+
+   cout<<"\n\n";
+   return 0;
+}
+
+/***** MEMBER FUNCTION DEFINITION *****/
+bool Stack::isFull()	{
+	if( top == (SIZE - 1) )	
+		return true;
+	else
+		return false;
+}
+
+bool Stack::isEmpty()	{
+	if( top == -1 )
+		return true;
+	else
+		return false;
+}
+
+void Stack::push(int data)	{	
+	if( isFull() )
+		OVERFLOW_MESSAGE;
+	else
+		A[++top] = data;
+}
+
+int Stack::pop()	{
+	if( isEmpty() )
+		UNDERFLOW_MESSAGE;
+	else
+		return A[top--];
+}
+
+int Stack::peek()	{
+	if( isEmpty() )
+		UNDERFLOW_MESSAGE;
+	else
+		return A[top];
+}
+
+void Stack::dispAll()	{
+	if( isEmpty() )
+		UNDERFLOW_MESSAGE;
+	else	{
+		for(int i = 0; i <= top; i++)
+			cout<<" "<<A[i];
+	}
+}
+
+void Stack::clear()	{
+	top = -1;
+}
